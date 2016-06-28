33facbbdfa51545ca194e3c20cf940c3c99a1719
Mike Smart <mike.smart@jadu.co.uk>
2016-06-28 23:55:37 +0100

    gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,85 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const mocha = require('gulp-mocha');
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+var browserSync = require('browser-sync');
+var scss = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var pixrem = require('gulp-pixrem');
+var rename = require('gulp-rename');
 
-const config = {
-    package: 'responsibleTable',
-    dir: {
-        dist: {
-            src: 'dist',
-            dest: 'public/dist'
-        }
-    },
-    tests: {
-        src: 'tests'
-    },
-    javascript: {
-        src: 'src',
-        dest: 'dist',
-        public: 'public/javascript'
+// bundle javascript
+function bundle (watch) {
+    var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel.configure({
+        presets: ["es2015"]
+    })));
+
+    function rebundle () {
+        bundler.bundle()
+            .on('error', function(err) { console.error(err); this.emit('end'); })
+            .pipe(source('build.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./public/dist'));
     }
+
+    if (watch) {
+        bundler.on('update', function() {
+            rebundle();
+        });
+    }
+
+    rebundle();
 }
 
-gulp.task('tests', () => {
-    return gulp.src(`${config.tests.src}/index.js`)
-            .pipe(mocha({
-                require: {
-                    chai: require('chai'),
-                    expect: require('chai').expect
-                },
-                globals: {
-                    chai: require('chai'),
-                    expect: require('chai').expect
-                }
-            }));
+// compile scss
+gulp.task('scss', function () {
+    return gulp
+        .src('./public/style/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer({ annotation: false, browsers: ['> 1%', 'last 5 versions', 'Firefox ESR', 'Opera 12.1'] }))
+        .pipe(pixrem({ baseFontSize: '16px' }))
+        .pipe(sourcemaps.write('maps/'))
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('./public/dist'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('javascript', () => {
-    return gulp.src(`${config.javascript.src}/*.js`)
-            .pipe(sourcemaps.init())
-            .pipe(babel({ presets: ['es2015'] }))
-            .pipe(uglify())
-            .pipe(rename(`${config.package}.min.js`))
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest(config.javascript.public))
-            .pipe(browserSync.stream());
+// watch
+// function watch () {
+//     return bundle(true);
+// }
+
+// dev server
+// function serve () {
+//
+// }
+
+gulp.task('javascript', function () {
+    return bundle();
 });
 
-gulp.task('serve', () => {
+gulp.task('serve', function () {
     browserSync.init({
-        open: false,
         server: {
-            baseDir: './public/'
-        }
+            baseDir: './',
+            index: './public/index.html'
+        },
+        open: false
     });
 
-    gulp.watch(`${config.javascript.src}/*.js`, ['javascript'], reload);
+    gulp.watch('./src/*.js', ['javascript'], browserSync.reload);
+    // watch SCSS
+    gulp.watch('./public/style/**/*.scss', ['scss'], browserSync.reload);
+    // watch HTML
+    gulp.watch('./public/**/*.html', browserSync.reload);
 });
 
-gulp.task('copy', () => {
-    return gulp.src(`${config.dir.dist.src}/**/*`)
-            .pipe(gulp.dest(`${config.javascript.public}`));
-});
+// gulp.task('watch', function () { return watch(); });
 
-gulp.task('default', [
-    'javascript',
-    'copy',
-    'serve'
-]);
+gulp.task('default', ['javascript', 'scss', 'serve']);

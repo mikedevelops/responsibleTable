ecdaa2c8134502bf942383e97aa9e994cf336d7f
Mike Smart <mike.smart@jadu.co.uk>
2016-08-04 17:52:20 +0100

    Fixing package.json main

--- src/responsibleTables.js
+++ src/responsibleTables.js
@@ -5,7 +5,7 @@
 
 class ResponsibleTables {
     constructor (options) {
-        ResponsibleTables.VERSION = '1.0.2';
+        ResponsibleTables.VERSION = '1.0.3';
 
         // defaults
         this.defaults = {
